7db5bef1e3e11687e25214a4d076a80ed1fcde17
Muhammad Fahad <muhammadfahadbinabid@gmail.com>
2024-02-02 15:22:33 +0500

    VS studio try

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API3._0.Dtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- obj/Debug/net7.0/API3.0.AssemblyInfo.cs
+++ obj/Debug/net7.0/API3.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API3.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32acc3520e905468cc46c3094aeb95379d41f946")]
 [assembly: System.Reflection.AssemblyProductAttribute("API3.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API3.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
